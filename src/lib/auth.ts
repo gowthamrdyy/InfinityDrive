db88a70cb5f02611aca543e2d907361124eba6e1
Gowtham sree charan reddy <iamgowthamsree@gmail.com>
2026-07-05 19:10:41 +0530

    fix: add trustHost to nextauth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,6 +10,7 @@ import Google from 'next-auth/providers/google';
 import { db } from './firebase';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
